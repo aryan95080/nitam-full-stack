75939daa2c5d4540ebe89434668db982cc646d82
Amit Kumar <amitarya95080@gmail.com>
2025-03-25 01:30:22 +0530

    In the product controller   , added the functionality to add product and listed all product

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,12 +2,15 @@ import {v2 as cloudinary} from "cloudinary"
 
 // Cloudinary configuration
 const connectCloudinary =async ()=>{
+ // console.log("Cloudinary Config:", process.env.CLOUDINARY_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+
+
   cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET,
     
-  })
+  });
 
-}
+};
 export default connectCloudinary;
